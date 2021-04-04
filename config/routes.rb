0f9b2fbb6d6b101acac5cb90012f1bed24c9e359
Karson <kars.kalt@gmail.com>
2021-04-04 14:02:20 -0400

    Added routes to contoller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :plants
   resources :users
-  resources :species
+  resources :species, only: [:index, :show]
+  resources :waterings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
