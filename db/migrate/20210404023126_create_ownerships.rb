9a3575adde2adc06150af96a518ec7bcdd423b90
Karson <kars.kalt@gmail.com>
2021-04-03 22:32:04 -0400

    Added Ownership model

---
+++ db/migrate/20210404023126_create_ownerships.rb
@@ -0,0 +1,11 @@
+class CreateOwnerships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ownerships do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :plant, null: false, foreign_key: true
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+  end
+end
