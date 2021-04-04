2a86d98c164a9f7bf0b08a2ee34d556c5e0d3091
Karson <kars.kalt@gmail.com>
2021-04-03 23:50:43 -0400

    Built validations, before_save hook for Plant.

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,17 @@ class Plant < ApplicationRecord
   has_many :waterings
   has_many :ownerships
   has_many :users, through: :ownerships
+
+  validates :nickname, length: {maximum: 20}
+  validates :creator_id, presence: true
+
+  before_save :inherit_water_frequency_from_species_if_nil
+
+  private
+
+  def inherit_water_frequency_from_species_if_nil
+    if water_frequency == nil
+      water_frequency = species.water_frequency
+    end
+  end
 end
