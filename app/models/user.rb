47537c82f13cb9a6896ec05a73081752e24f31ba
Karson <kars.kalt@gmail.com>
2021-04-03 23:53:54 -0400

    Built validations for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_many :ownerships
     has_many :plants, through: :ownerships
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: {in: 4..15}
+
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
