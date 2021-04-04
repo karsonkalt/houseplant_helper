ffc3f51881e2adab90de1ea3e916f407be76e777
Karson <kars.kalt@gmail.com>
2021-04-03 22:58:41 -0400

    Set up relationships in models

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,6 @@
 class Plant < ApplicationRecord
   belongs_to :species
+  has_many :waterings
   has_many :ownerships
   has_many :users, through: :ownerships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :ownerships
+    has_many :plants, through: :ownerships
 end
