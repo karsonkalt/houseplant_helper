dee79df2a7d9be879ef2dec3c6a3e176369aeaba
Karson <kars.kalt@gmail.com>
2021-04-03 23:56:36 -0400

    Built validations for Species.

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,2 +1,6 @@
 class Species < ApplicationRecord
+    validates :common_name, presence: true
+    validates :water_frequency, presence: true
+    validates :image_path, presence: true
+    validates :notes, presence: true
 end
