28f55acdecc40cf8dacde52e869774279ab882b3
Karson <kars.kalt@gmail.com>
2021-04-05 21:29:11 -0400

    Fixed  bug with user_params that didn't accept :username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(:name, :email)
+        params.require(:user).permit(:username, :email)
     end
 
 end
