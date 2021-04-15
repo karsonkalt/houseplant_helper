591ca194658e8d7d57b66c6c83172efdd7b62665
Karson <kars.kalt@gmail.com>
2021-04-14 20:11:27 -0400

    Added styling to error handling pages.

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
 end
